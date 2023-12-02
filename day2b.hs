4ee3eb1d613e82a9b746a30fbb25010b06407c04
Zdeněk Biberle <z.biberle@gmail.com>
2023-12-02 22:32:46 +0100

    Day 2, second problem: Shaved off two bytes

--- day2b.hs
+++ day2b.hs
@@ -2,5 +2,5 @@ import Data.List.Split
 import Data.Map
 s=splitOn
 g[n,c]=(c,read n)
-f[a,b]=product$elems$fromListWith max$fmap(g.s" ")$s", "b>>=s"; "
+f[a,b]=product$elems$fromListWith max$g.s" "<$>(s", "b>>=s"; ")
 main=interact$show.sum.fmap(f.s": ").lines
