766d19aa8ffd4fbb9156ff3e6e95b38e701a32eb
Zdeněk Biberle <z.biberle@gmail.com>
2024-01-16 19:28:40 +0100

    Day 11

---
+++ day11b.hs
@@ -0,0 +1,5 @@
+import Data.List
+i!l=i+head([1|'#'<-l]<>[10^6])
+t=scanl(!)0
+f w=sum[abs(a-c)+abs(b-d)|((a,b):p)<-tails[(x,y)|(y,l)<-zip(t w)w,(x,'#')<-zip(t$transpose w)l],(c,d)<-p]
+main=interact$show.f.lines
